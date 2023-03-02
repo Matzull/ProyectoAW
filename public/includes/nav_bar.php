3e6443cec3674f768f8ca08788a70de1287b25c6
Diego <diegobqu@ucm.es>
2023-03-02 21:57:42 +0100

    nav-bar básico

---
+++ public/includes/nav_bar.php
@@ -0,0 +1,24 @@
+<div id="nav-bar-container">
+    <div id="nav-bar">
+        <img src="/img/logo-dMode.png" alt="" height="50">
+        <ul>
+            <li>Inicio</li>
+            <li>Mercado de Kernels
+                <ul>
+                    <div class="dropdown-content">
+                        <li>Mercado de Kernels</li>
+                        <li>Ranking</li>
+                    </div>
+                </ul>
+            </li>
+            <li>Información
+                <ul>
+                    <div class="dropdown-content">
+                        <li>FAQ</li>
+                        <li>Contacto</li>
+                    </div>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
--- public/user_dashboard.php
+++ public/user_dashboard.php
@@ -7,9 +7,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/nav_bar.css">
 </head>
 
-<body>
+<body style="background-color: var(--bg); margin: 0px">
+    <?php require_once("./includes/nav_bar.php") ?>
     logged:
     <?php echo $_SESSION["user_email"] ?>
 </body>
