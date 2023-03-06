9b38a90d0e66e5ee49f28f7e785cdd0363cdac7b
jerez <jjerez01@ucm.es>
2023-03-06 11:21:50 +0100

    aniadido footer

---
+++ public/includes/footer.php
@@ -0,0 +1,33 @@
+<footer>
+
+	<h3>Contacto</h3>
+		<dl>
+			<dt><a href="index.php"> Danos tu opinión</a></li>
+			<dt><a href="contenido.php"> Redes sociales</a></li>
+
+			<!-- TODO poner iconos de redes sociales -->
+		</dl>
+
+	<h3>Informacion</h3>
+		<dl>
+			<dt><a href="index.php"> FAQ</a></li>
+		</dl>
+
+	<h3>Mercado de kernels</h3>
+		<dl>
+			<dt><a href="index.php">kernels mejor pagados</a></li>
+			<dt><a href="contenido.php">kernels nuevos</a></li>
+		</dl>
+
+	<h3>Rankings</h3>
+		<dl>
+			<dt><a href="index.php">Ranking de kernels</a></li>
+			<dt><a href="contenido.php">Mejores usuarios</a></li>
+		</dl>
+</footer>
+
+<?php
+
+
+
+?>
