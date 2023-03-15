29aa0abd3e45c115ce3e89a00ad04cb2ee5ba26e
jerez <jjerez01@ucm.es>
2023-03-15 20:02:31 +0100

    footer hecho

--- public/kernel_marketplace.php
+++ public/kernel_marketplace.php
@@ -10,7 +10,8 @@
     <link rel="stylesheet" href="css/nav_bar.css">
     <link rel="stylesheet" href="css/user_nav_bar.css">
     <link rel="stylesheet" href="css/user_dashboard.css">
-    <link rel="stylesheet" href="css/contacto.css">
+    <link rel="stylesheet" href="css/marketplace.css">
+    <link rel="stylesheet" href="css/footer.css">
 </head>
 
 <body>
@@ -80,7 +81,10 @@
         
     </div>
     
-
+    <?php
+    
+    require_once("./includes/vistas/footer.php");
+    ?>
 
 </body>
 
