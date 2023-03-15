2285bbf20505665d085ff40825f5297bbef3d847
Jantri-3 <trillocarrerasjuan@gmail.com>
2023-03-15 11:30:32 +0100

    Arreglado del todo contacto?

--- public/contacto.php
+++ public/contacto.php
@@ -17,7 +17,6 @@ if (!isset($_SESSION['user'])) {
     <title>Contacto</title>
     <link rel="stylesheet" href="css/nav_bar.css">
     <link rel="stylesheet" href="css/user_nav_bar.css">
-    <link rel="stylesheet" href="css/user_dashboard.css">
     <link rel="stylesheet" href="css/contacto.css">
 </head>
 
@@ -26,42 +25,41 @@ if (!isset($_SESSION['user'])) {
     require_once "includes/config.php";
     require_once("./includes/vistas/nav_bar.php");
     ?>
-    <div class="form-container">
+   
+    <div class="container">
         <div class="form">
             <h2 class="title">
                 Contacto
             </h2>
             <p class="subtitle t-muted">Danos feedback de nuestra web!</p>
             <?php
-
             if (isset($_SESSION["user_email"])) {
                 $formulario = new \parallelize_namespace\formulario\FormularioContacto();
                 echo $formulario->gestiona();
                 
+
             } else {
                 echo "<p>Necesitas estar registrado para enviarnos información, registrate aqui o si ya tienes cuenta haz login!</p>"; // TODO los enlaces
             }
             ?>
-
         </div>
-
-        <div id="socials" class="form">
-            <h2 class="title">
-                Redes sociales
-            </h2>
-            <p class="subtitle t-muted">Síguenos en nuestras redes!</p>
-
-            <!-- Creamos redes sociales? -->
-            <a href="https://instagram.com"><img src="img/logo-Instagram-bMode.png"></a>
-            <a href="https://facebook.com"><img src="img/logo-Facebook-bMode.png"></a>
-            <a href="https://github.com"><img src="img/logo-Github-dMode.png"></a>
-
+        <div class="socials-cont">
+            <div id="socials" class="form">
+                    <h2 class="title">
+                        Redes sociales
+                    </h2>
+                    <p class="subtitle t-muted">Síguenos en nuestras redes!</p>
+                    <!-- Creamos redes sociales? -->
+                    <a href="https://instagram.com"><img class="small" src="img/logo-Instagram-bMode.png"></a>
+                    <a href="https://facebook.com"><img class="small" src="img/logo-Facebook-bMode.png"></a>
+                    <a href="https://github.com"><img class="small" src="img/logo-Github-dMode.png"></a>
+            </div>
+            <div class="social-drawing">
+                    <!-- dibujo blobs/ Flor -->
+            </div>
         </div>
-        <div class="form-drawing">
-            <!-- dibujo blobs/ Flor -->
-        </div>
+        
     </div>
-
 
 </body>
 
