abc25f7c952c722ffaafff845b66fb62d42da068
Jaimevzkz <79526840+Jaimevzkz@users.noreply.github.com>
2023-03-15 10:04:54 +0100

    vistas

---
+++ public/FAQ.php
@@ -0,0 +1,36 @@
+<?php
+namespace parallelize_namespace;
+
+require 'includes/config.php';
+
+?>
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>FAQ</title>
+    <link rel='stylesheet' href='/css/nav_bar.css'>
+    <link rel="stylesheet" href="css/FAQ.css">
+</head>
+
+<body>
+    <?php require('./includes/vistas/nav_bar.php') ?>
+    <div class="form">
+        <h1 class="title">
+            FAQ
+        </h1>
+        <input type="text" placeholder="¿Cómo te podemos ayudar">
+    </div>
+
+    <h5>Kernels</h5>
+    <h5>Otro tema</h5>
+    <h5>Otro tema</h5>
+    <h5>Otro tema</h5>
+    <h5>Otro tema</h5>
+
+</body>
+
+</html>
