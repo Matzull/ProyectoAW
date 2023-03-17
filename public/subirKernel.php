64d0fdbebaed3888630b369efaa06b74909d70b1
jjerez <jjerez01@ucm.es>
2023-03-17 15:12:57 +0100

    pequenio ajuste

--- public/subirKernel.php
+++ public/subirKernel.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['user_email'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contacto</title>
+    <title>Subir kernel</title>
     <link rel="stylesheet" href="css/nav_bar.css">
     <link rel="stylesheet" href="css/user_nav_bar.css">
     <link rel="stylesheet" href="css/contacto.css">
