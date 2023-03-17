1b998c11fbb51b6d0d921d3a22c5363146a27daf
jjerez <jjerez01@ucm.es>
2023-03-17 11:34:10 +0100

    formulario kernel v2

--- public/includes/formularios/FormularioSubirKernel.php
+++ public/includes/formularios/FormularioSubirKernel.php
@@ -9,13 +9,28 @@ class FormularioSubirKernel extends Formulario
     protected function generaCamposFormulario(&$datos = array())
     {
 
-        $val_user_comment = isset($datos['user_comment']) ? $datos['user_comment'] : '';
+        $val_kernel_name = isset($datos['kernel_name']) ? $datos['kernel_name'] : '';
+        $val_input_kernel = isset($datos['input_kernel']) ? $datos['input_kernel'] : '';
+        $val_kernel_description = isset($datos['kernel_description']) ? $datos['kernel_description'] : '';
+        $val_input_price = isset($datos['input_price']) ? $datos['input_price'] : '';
 
         return generaErroresGlobalesFormulario($this->errores) . <<<HTML
+
+        <label for = 'kernel_name'>Nombre del kernel</label>
+        <textarea id = 'kernel_name' class = 'input-field' name = 'kernel_name' placeholder = 'Nombre del kernel' value = "$val_kernel_name" ></textarea>
+        HTML . generarError('kernel_name', $this->errores) . <<<HTML
         
-        <label for = 'user_comment'>Comentarios</label>
-        <textarea id = 'user_comment' class = 'input-field' name = 'user_comment' placeholder = 'Escribe cualquier cosa que quieras comentarnos' value = "$val_user_comment" ></textarea>
-        HTML . generarError('user_comment', $this->errores) . <<<HTML
+        <label for = 'input_kernel'>Introducir kernel</label>
+        <textarea id = 'input_kernel' class = 'input-field' name = 'input_kernel' placeholder = 'Introduce tu kernel' value = "$val_input_kernel" ></textarea>
+        HTML . generarError('input_kernel', $this->errores) . <<<HTML
+
+        <label for = 'kernel_description'>Descripcion del kernel</label>
+        <textarea id = 'kernel_description' class = 'input-field' name = 'kernel_description' placeholder = 'Pon una breve descripcion del funcionamiento del kernel' value = "$val_kernel_description" ></textarea>
+        HTML . generarError('kernel_description', $this->errores) . <<<HTML
+
+        <label for = 'input_price'>Precio</label>
+        <textarea id = 'input_price' class = 'input-field' name = 'input_price' placeholder = 'Pon un precio a pagar por la ejecucion' value = "$val_input_price" ></textarea>
+        HTML . generarError('input_price', $this->errores) . <<<HTML
       
         <!-- la id de este button era create-acc-button -->
         <button id = 'send-button' class = 'button c-h-blue' type = 'submit' title = 'Aceptar' name = 'Aceptar'>Aceptar</button>
@@ -26,9 +41,25 @@ class FormularioSubirKernel extends Formulario
 
     protected function procesaFormulario(&$datos)
     {
+        
+        $kernel_name = filter_input(INPUT_POST, 'kernel_name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        if (!$kernel_name || empty($kernel_name = trim($kernel_name))) {
+            $this->errores['kernel_name'] = 'Debes poner un nombre a tu kernel.';
+        }
+
         $input_kernel = filter_input(INPUT_POST, 'input_kernel', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         if (!$input_kernel || empty($input_kernel = trim($input_kernel))) {
-            $this->errores['input_kernel'] = 'El kernel es erroneo.';
+            $this->errores['input_kernel'] = 'Debes introducir codigo.';
+        }
+
+        $kernel_description = filter_input(INPUT_POST, 'kernel_description', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        if (!$kernel_description || empty($kernel_description = trim($kernel_description))) {
+            $this->errores['kernel_description'] = 'Debes poner una descripcion de tu kernel.';
+        }
+
+        $input_price = filter_input(INPUT_POST, 'input_price', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        if (!$input_price || empty($input_price = trim($input_price))) {
+            $this->errores['input_price'] = 'Debes poner un precio a tu kernel.';
         }
 
         if (!isset($_SESSION["user"])) {
