8f386b244c45bd308d0d347939628f2d216a42ee
Jantri-3 <trillocarrerasjuan@gmail.com>
2023-03-27 11:51:48 +0200

    Tocada la CSS de token_transaction
    Falta un justify por ahi en el formde transaction

--- public/token_transaction.php
+++ public/token_transaction.php
@@ -5,7 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Transaction</title>
+
+    <link rel="stylesheet" href="css/token_transaction.css">
+    <link rel="stylesheet" href="css/nav_bar.css">
+    <link rel="stylesheet" href="css/user_nav_bar.css">
+    <link rel="stylesheet" href="css/contacto.css">
 </head>
 
 <body>
@@ -18,11 +23,16 @@
     if (!isset($_SESSION["user_email"])) {
         echo '<p>Debes haberte identificado</p>';
     } else {
+       echo ' <div class="form-container">';
+       echo ' <div class="form">';
         $usuario = \parallelize_namespace\Usuario::buscaUsuario($_SESSION["user_email"]);
         $t = $usuario->gettoekns();
-        echo "<p>actualmente tienes $t tokens</p>";
+        echo '<h2 class="title">Tokens</h2>';
+        echo "<p class=\"subtitle t-muted\">Actualmente tienes $t tokens</p>";
         $formulario = new \parallelize_namespace\formulario\FormularioTransaction();
         echo $formulario->gestiona();
+        echo ' </div>';
+        echo ' </div>';
     }
 
     ?>
