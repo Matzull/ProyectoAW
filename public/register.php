81569277802939598adcd96f1d64c8a158d0da10
MARCOS ALONSO CAMPILLO <marcal16@ucm.es>
2023-03-29 11:56:34 +0200

    Fixed config import in register

--- public/register.php
+++ public/register.php
@@ -1,4 +1,5 @@
 <!-- Define que el documento esta bajo el estandar de HTML 5 -->
+<?php require_once "includes/config.php";?>
 <!DOCTYPE html>
 
 <!-- Representa la raíz de un documento HTML o XHTML. Todos los demás elementos deben ser descendientes de este elemento. -->
@@ -15,7 +16,6 @@
 
 <body>
     <?php
-    require_once "includes/config.php";
     $formulario = new \parallelize_namespace\formulario\FormularioRegister();
     ?>
     <div class="form-container">
