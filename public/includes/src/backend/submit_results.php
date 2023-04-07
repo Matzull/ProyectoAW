9b37e1ad25174a103f9bf5cde78739927884093f
jaime gonzalez fabregas <jaimegonzalezfabregas@gmail.com>
2023-04-07 17:20:28 +0200

    avances en el tema de la restructuracion del resultado, pero parece que no se transmite bien el resultado de vuelta

--- public/includes/src/backend/submit_results.php
+++ public/includes/src/backend/submit_results.php
@@ -5,6 +5,6 @@ require '../../config.php';
 $kernel_id = $_GET["id"];
 $start = $_GET["start"];
 $end = $_GET["end"];
-$results = $_GET["res"];
+$results = $_POST["res"];
 
 \parallelize_namespace\ExecutionSegment::completaSegmento($start, $end, $kernel_id, $results);
--- public/kernel_info.php
+++ public/kernel_info.php
@@ -30,20 +30,20 @@ require 'includes/config.php';
 <body>
     <?php
     require_once("./includes/src/vistas/nav_bar.php");
-
     ?>
-    <div class="flex-container-info horizontal">
-        <div class="codeBlock">
-            <h2 class="title">
-                Codigo fuente
-            </h2>
-            <?php
-            $kernel = \parallelize_namespace\Kernel::buscaKernelPorId($_GET["id"]);
-            ?>
-            <pre id="sourceCode">
+    <div class="flex-container-info vertical">
+        <div class="flex-container-info horizontal">
+            <div class="codeBlock">
+                <h2 class="title">
+                    Codigo fuente
+                </h2>
+                <?php
+                $kernel = \parallelize_namespace\Kernel::buscaKernelPorId($_GET["id"]);
+                ?>
+                <pre id="sourceCode">
                 <?= $kernel->getCode() ?>
             </pre>
-            <!-- <script type="text/javascript"> // levatelo a otro archivo porfa thanks
+                <!-- <script type="text/javascript"> // levatelo a otro archivo porfa thanks
                 var code = "<?php echo addcslashes(html_entity_decode($kernel->getCode(), ENT_QUOTES), "\n") ?>";
                 var editor = CodeMirror(document.getElementById("sourceCode"), {
                     lineNumbers: true,
@@ -57,39 +57,82 @@ require 'includes/config.php';
                 editor.setValue(code);
                 editor.setSize("100%", "100%");
             </script>   -->
-        </div>
-
-        <div class="flex-container-info vertical block">
-            <div class="form">
-                <h2 class="title centered">
-                    <?= $kernel->getname(); ?>
-                </h2>
-                <p>Usuario:
-                    <?= \parallelize_namespace\Usuario::buscaUsuario($kernel->getuser_email())->getName() ?>
-                </p>
-                <p>Estado:
-                    <?= $kernel->getis_finished() == 0 ? "Computing" : "Finished" ?>
-                </p>
-                <p>Recompensa:
-                    <?= $kernel->getreward_per_line() ?> tk/line
-                </p>
-                <p class="form">
-                    <?= $kernel->getdescription() ?>
-                </p>
             </div>
-            <div class="form block">
 
-                <div class="flex-container-info ">
-                    <button id="btn_box" class="small-button fill-flex transition"
-                        onclick="comenzarEjecucion(<?= $_GET["id"] ?>)">
-                        <p id="btn_text">Ejecutar</p>
-                    </button>
+            <div class="flex-container-info vertical block">
+                <div class="form">
+                    <h2 class="title centered">
+                        <?= $kernel->getname(); ?>
+                    </h2>
+                    <p>Usuario:
+                        <?= \parallelize_namespace\Usuario::buscaUsuario($kernel->getuser_email())->getName() ?>
+                    </p>
+                    <p>Estado:
+                        <?= $kernel->getis_finished() == 0 ? "Computing" : "Finished" ?>
+                    </p>
+                    <p>Recompensa:
+                        <?= $kernel->getreward_per_line() ?> tk/line
+                    </p>
+                    <p class="form">
+                        <?= $kernel->getdescription() ?>
+                    </p>
+                </div>
+                <div class="form block">
+
+                    <div class="flex-container-info ">
+                        <button id="btn_box" class="small-button fill-flex transition"
+                            onclick="comenzarEjecucion(<?= $_GET["id"] ?>)">
+                            <p id="btn_text">Ejecutar</p>
+                        </button>
+                    </div>
+                    <p id="state_text"></p>
+
                 </div>
             </div>
         </div>
+        <?php
+        if (isset($_SESSION["user_email"]) && $kernel->getuser_email() == $_SESSION["user_email"]) {
+            ?>
+            <div>
+                <h1 class="centered_text">Información privada para el propietario</h1>
+                <div class="codeBlock">
+
+                    <a download="resultados_de_kernel.csv" href="data:text/html,<?php
+
+
+                    function result_at($n)
+                    {
+                        $segments = \parallelize_namespace\ExecutionSegment::buscaSegmentosConKernelId($_GET["id"]);
+
+                        foreach ($segments as $i => $seg) {
+                            if ($seg->contains($n) && isset(explode(",", $seg->getresults())[$n])) {
+                                return explode(",", $seg->getresults())[$n];
+                            }
+                        }
+                        return "";
+                    }
+
+                    for ($iteration = 0; $iteration < $kernel->getiteration_count(); $iteration++) {
+                        echo result_at($iteration);
+                        if ($iteration != $kernel->getiteration_count() - 1) {
+                            echo ",";
+                        }
+                    }
+
+                    ?>">Descargar resultados
+                        <?= $kernel->getis_finished() ? "totales" : "parciales" ?>
+                    </a>
+
+                </div>
+            </div>
+        <?php } ?>
     </div>
 
-    <script src="js/kernel_execution.js"></script>
+
+    <?php require "js/kernel_execution.php"; ?>
+
+    <script src="js/work_coordination.js"></script>
+
 
 </body>
 <?php require_once("./includes/src/vistas/footer.php"); ?>
