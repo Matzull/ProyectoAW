78954b8c0c87b9fbba625908828825dde5afaab5
Diego <diegobqu@ucm.es>
2023-04-12 15:56:56 +0200

    token transaction retiro e ingreso

--- public/includes/src/formularios/FormularioTransaction.php
+++ public/includes/src/formularios/FormularioTransaction.php
@@ -6,17 +6,19 @@ require_once 'includes/config.php';
 
 class FormularioTransaction extends Formulario
 {
+    private bool $withdraw;
+
     protected function generaCamposFormulario(&$datos = array())
     {
 
         $val_token_delta = isset($datos['token_delta']) ? $datos['token_delta'] : '';
-
+        $button_text = $this->withdraw? 'Retirar Tokens': 'Ingresar Tokens';
         return generaErroresGlobalesFormulario($this->errores) . <<<HTML
-        <label for = 'token_delta'>Volumen de tokens</label>
-        <input id = 'token_delta' class = 'input-field' type = 'text' name = 'token_delta' placeholder = '123' value = "$val_token_delta" required>
+        <label for='token_delta'>Volumen de tokens</label>
+        <input id='token_delta' class='input-field' type='text' name='token_delta' placeholder='123' value="$val_token_delta" required>
         HTML . generarError('token_delta', $this->errores) . <<<HTML
         
-        <button id = 'create-acc-button' class = 'button c-h-blue' type = 'submit' title = 'Pedir' name = 'Pedir'>Pedir transacción</button>
+        <button id='create-acc-button' class='button c-h-blue' type='submit' title='Pedir' name='Pedir'>$button_text</button>
 
         HTML;
     }
@@ -32,21 +34,24 @@ class FormularioTransaction extends Formulario
             $this->errores[] = 'Debes haberte identificado';
         }
         $usuario = \parallelize_namespace\Usuario::buscaUsuario($_SESSION["user_email"]);
-
-        if ($usuario->gettokens() + $token_delta < 0) {
+        $tokens = $this->withdraw? $usuario->gettokens() - $token_delta: $usuario->gettokens() + $token_delta;
+        
+        if ($tokens < 0) {
             $this->errores['token_delta'] = 'Debes tener suficiente dinero!';
         }
 
         if (count($this->errores) === 0) {
-            echo $token_delta . ":" . $usuario->gettokens() + $token_delta;
-            $usuario->setTokens($usuario->gettokens() + $token_delta);
-            \parallelize_namespace\Transaction::submit($token_delta, $_SESSION["user_email"], "movimiento bancario", $usuario->gettokens());
+            echo $token_delta . ":" . $tokens;
+            $usuario->setTokens($tokens);
+            \parallelize_namespace\Transaction::submit($this->withdraw? -$token_delta: $token_delta, $_SESSION["user_email"], "movimiento bancario", $usuario->gettokens());
         }
 
     }
 
-    public function __construct()
+    public function __construct(bool $withdraw)
     {
-        parent::__construct(0, array('action' => './token_transaction.php', 'method' => 'POST', 'urlRedireccion' => './wallet.php'));
+        parent::__construct(0, array('action' => ($withdraw? './token_transaction.php?withdraw=true': './token_transaction.php'),
+            'method' => 'POST', 'urlRedireccion' => './wallet.php'));
+        $this->withdraw = $withdraw;
     }
 }
--- public/js/user_nav_bar.js
+++ public/js/user_nav_bar.js
@@ -9,14 +9,4 @@ function toggleUserNavBar(){
         userNavBar.classList.add('nb-closed');
         document.getElementById('user-panel').classList.add('nb-closed');
     }
-    // if (panel.style.height == '0px'){
-    //     panel.style.height = '250px';
-    //     document.getElementById('user-nb-toggle-b').style.transform = 'scaleY(1)';
-    //     document.getElementById('user-panel').style.marginRight = '215px';
-    // }
-    // else{
-    //     panel.style.height = '0px';
-    //     document.getElementById('user-nb-toggle-b').style.transform = 'scaleY(-1)';
-    //     document.getElementById('user-panel').style.marginRight = '0px';
-    // }
 }
