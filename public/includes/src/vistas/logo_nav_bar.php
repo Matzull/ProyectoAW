af49921a3d0a1fde5c0d29352d9e1f2154620568
Diego <diegobqu@ucm.es>
2023-04-14 16:33:48 +0200

    responsive login y crear cuenta

--- public/includes/src/vistas/logo_nav_bar.php
+++ public/includes/src/vistas/logo_nav_bar.php
@@ -1,12 +1,3 @@
 <div id="logo-nv">
     <a href="index.php"><img src="<?= RUTA_IMGS ?>/logo-h-dMode.png" alt="" height="30"></a>
 </div>
-<style>
-    #logo-nv {
-        position: absolute;
-        top: 0px;
-        padding: 20px;
-        width: 100%;
-        box-sizing: border-box;
-    }
-</style>
--- public/register.php
+++ public/register.php
@@ -7,9 +7,12 @@
 
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Crear Cuenta </title>
     <meta name="keywords" content="Crear Cuenta">
     <!-- Link hacia el archivo de estilos css -->
+    <link rel="stylesheet" href="<?= RUTA_CSS ?>/logo_nav_bar.css">
     <link rel="stylesheet" href="<?= RUTA_CSS ?>/global.css">
     <link rel="stylesheet" href="<?= RUTA_CSS ?>/register.css">
 </head>
@@ -19,24 +22,25 @@
     $formulario = new \parallelize_namespace\formulario\FormularioRegister();
     require "includes/src/vistas/logo_nav_bar.php";
     ?>
-    <div class="form-container">
-        <div class="form">
-            <h2 class="title">
-                Crear Cuenta
-            </h2>
-            <p class="subtitle t-muted">Crea una cuenta para empezar...</p>
+    <div class="main-container">
+        <div class="form-container">
+            <div class="form">
+                <h2 class="title">
+                    Crear Cuenta
+                </h2>
+                <p class="subtitle t-muted">Crea una cuenta para empezar...</p>
 
-            <?= $formulario->gestiona() ?>
+                <?= $formulario->gestiona() ?>
 
-            <p class="text-sec-button">¿Ya tienes cuenta?</p>
-            <button class="button c-h-b-blue"  id="loginbutton" onclick="location.href='login.php'">
-                Iniciar Sesión</button>
-        </div>
-        <div class="form-drawing">
-            <img src="svg/blobanimation.svg">
+                <p class="text-sec-button">¿Ya tienes cuenta?</p>
+                <button class="button c-h-b-blue"  id="loginbutton" onclick="location.href='login.php'">
+                    Iniciar Sesión</button>
+            </div>
+            <div class="form-drawing">
+                <img src="svg/blobanimation.svg">
+            </div>
         </div>
     </div>
-
 </body>
 
 </html>
